615489478de1559aca9d9958817ef3fa50678bf6
Matszak <mtracz2@edu.cdv.pl>
2025-03-25 16:26:07 +0100

    Added Howering Card in deck

--- Assets/Cards/CardsInGame/UICardUsage.cs
+++ Assets/Cards/CardsInGame/UICardUsage.cs
@@ -3,12 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
 public class UICardUsage : MonoBehaviour,IPointerEnterHandler, IPointerExitHandler
 {
 
     private Card _card;
-
+    public Vector2 targetPosition;
+    
+    private Canvas canvas;
+    
+    private bool _isDragging;
+    private bool _isHovering;
+    
+    private Vector2 _originalPosition;
+    
+    [SerializeField] float hoverDuration = 10;
+    public void Start()
+    {
+        _originalPosition = transform.position;
+    }
+    
     public void SetCard(Card card)
     {
         _card = card;
@@ -16,12 +31,22 @@ public class UICardUsage : MonoBehaviour,IPointerEnterHandler, IPointerExitHandl
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        _isHovering = true;
+        if (!_isDragging)
+        {
+            transform.DOMove(new Vector2(_originalPosition.x, _originalPosition.y + 150f), hoverDuration, true);
+        }
         Debug.Log($"Mouse is over the card: {_card.nameText}");
     }
 
     // This method is called when the mouse exits the UI element
     public void OnPointerExit(PointerEventData eventData)
     {
+        _isHovering = false;
+        if (!_isDragging)
+        {
+            transform.DOMove(_originalPosition, hoverDuration);
+        }
         Debug.Log($"Mouse exited the card: {_card.nameText}");
     }
 }

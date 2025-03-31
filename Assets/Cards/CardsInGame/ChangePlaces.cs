8a5d1c9203598ce57f27f815e7ff0d33199e14bd
WojtekM113 <wojtek.mendlik@gmail.com>
2025-03-31 19:02:40 +0200

    a

--- Assets/Cards/CardsInGame/ChangePlaces.cs
+++ Assets/Cards/CardsInGame/ChangePlaces.cs
@@ -14,16 +14,25 @@ namespace Cards.CardsInGame
             _currentPlayer = currentPlayer;
             GameManager.Instance.OnCardPlayerSelected += ApplyEffect;
             GameManager.Instance.InvokeSelection(currentPlayer);
+            
         }
 
         private void ApplyEffect(Player  selectedPlayer)
         {
+            var playerPosition = selectedPlayer.PlayerObject.transform.position;
+            var tempIndex = _currentPlayer.TileIndex;
             var tempPosition = _currentPlayer.PlayerObject.transform.position;
-            
-            _currentPlayer.PlayerObject.transform.position = selectedPlayer.PlayerObject.transform.position;
+
+            _currentPlayer.PlayerObject.transform.position = playerPosition;
             selectedPlayer.PlayerObject.transform.position = tempPosition;
             
+            _currentPlayer.TileIndex = selectedPlayer.TileIndex;
+            selectedPlayer.TileIndex = tempIndex;
+            
+                
+            
             GameManager.Instance.OnCardPlayerSelected -= ApplyEffect;
+            GameManager.Instance.TurnEnded(_currentPlayer);
         }
         
         
